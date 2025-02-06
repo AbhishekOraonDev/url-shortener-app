853a6fa8b17e484db1eba6fa3ca2321c8a94d5da
Abhishek Oraon <abhishekoraon082@gmail.com>
2025-02-06 07:03:24 +0530

    changed redis connection

--- config/redisClient.js
+++ config/redisClient.js
@@ -6,8 +6,8 @@ dotenv.config();
 // Use the Redis service name from Docker Compose
 const redisClient = new Redis({
     host: process.env.REDIS_HOST,
-    port: process.env.REDISPORT || 6379,
-
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD,
     retryStrategy: (times) => {
         const delay = Math.min(times * 50, 2000);
         return delay;
@@ -16,6 +16,7 @@ const redisClient = new Redis({
     enableReadyCheck: true,
     showFriendlyErrorStack: true
 });
+
 
 // Event Listeners
 redisClient.on("connect", () => {
